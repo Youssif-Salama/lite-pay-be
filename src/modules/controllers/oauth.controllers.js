040f1ee675683f941b0d70f5e433204f249b5545
YOUSSIF_SALAMA <128246586+Youssif-Salama@users.noreply.github.com>
2024-12-15 17:16:06 +0200

    Update oauth.controllers.js

--- src/modules/controllers/oauth.controllers.js
+++ src/modules/controllers/oauth.controllers.js
@@ -3,8 +3,7 @@ import { ErrorHandlerService } from "../../services/ErrorHandler.services.js";
 import { makeToken } from "../../utils/jwt/jwt.utils.js";
 
 export const addNewGoogleLoggedInUser= ErrorHandlerService(async (req, res) => {
-  const { email, name, googleId, accessToken } = req.data;
-  const lang=req.lang;
+  const { email, name, googleId, accessToken,lang } = req.data;
   // check if this email has a normal account
   const checkIfEmailUsed = await userModel.findOne({ where: { email } });
   if (!checkIfEmailUsed) {
@@ -16,13 +15,13 @@ export const addNewGoogleLoggedInUser= ErrorHandlerService(async (req, res) => {
     if (!signUpNewUser)
       throw new AppErrorService(400, "failed to create user");
     const token = makeToken({ user: signUpNewUser, accessToken });
-    res.redirect(`http://localhost:3000/${lang}/login/token=${token}`);
+    res.redirect(process.env.Frontend_Link+"/"+lang+"/login?token="+token)
     // res.status(201).json({ message: "signup success", token });
   } else {
     if (checkIfEmailUsed?.googleId) {
       // on found user exist with oauth then login him
       const token = makeToken({ user: checkIfEmailUsed, accessToken });
-    res.redirect(`http://localhost:3000/${lang}/login/token=${token}`);
+      res.redirect(process.env.Frontend_Link+"/"+lang+"/login?token="+token)
       // res.status(200).json({ message: "login success", token });
     } else {
       // on found user exist with normal account
